1cb9404adf0b5c93ad1eb068941834aa8c25ad54
Naurah Kamilia <if23.naurahkamilia@mhs.ubpkarawang.ac.id>
2025-06-30 16:40:26 +0700

    Delete storage directory

--- storage/framework/views/8f509524d84c667c31edf474e05bb7e9.php
+++
@@ -1,4 +0,0 @@
-<footer class="footer">
-  &copy; 2025 Admin Panel. All rights reserved.
-</footer>
-<?php /**PATH C:\laragon\www\coba_laravel\resources\views/partials/footer.blade.php ENDPATH**/ ?>
